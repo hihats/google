2a659ec67b74887dc5822608d41ab89aeded93c3
Hisahiro Tsukamoto <h_tsukamoto@innovation.co.jp>
2016-03-22 14:05:29 +0900

    add sample configuration file

---
+++ analytics/config/simple.php
@@ -0,0 +1,8 @@
+<?php
+return [
+  "view_ids" => [12345678],
+  "dimensions" => 'ga:pagePath,ga:date',
+  "metrics" => "ga:pageviews,ga:sessions,ga:users,ga:bounceRate",
+  "filters" => "ga:pageviews>100",
+  "sort" => "ga:date"
+];
